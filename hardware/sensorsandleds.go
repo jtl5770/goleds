1c9a4739c1ce30aed2942292572be6570f66b3de
Jens Lautenbacher <jtl@gmx.com>
2021-09-04 12:14:07 +0200

    prepared for led split into two parts

--- hardware/sensorsandleds.go
+++ hardware/sensorsandleds.go
@@ -7,14 +7,15 @@ import (
 )
 
 const LEDS_TOTAL = 125
-
-var Sensors = []Sensor{{0, 0, 0}, {69, 0, 7}, {70, 1, 0}, {124, 1, 5}}
+const _LEDS_SPLIT = 70
 
 type Sensor struct {
 	LedIndex int
 	Adc      int
 	AdcIndex int
 }
+
+var Sensors = []Sensor{{0, 0, 0}, {69, 0, 7}, {70, 1, 0}, {124, 1, 5}}
 
 func NewSensor(ledIndex int, adc int, adcIndex int) Sensor {
 	return Sensor{LedIndex: ledIndex, Adc: adc, AdcIndex: adcIndex}
@@ -29,7 +30,18 @@ func DisplayDriver(display chan ([]byte)) {
 		tmp.Grow(LEDS_TOTAL + 1)
 
 		sumLeds := <-display
-		for _, v := range sumLeds {
+		led1 := sumLeds[0:_LEDS_SPLIT]
+		led2 := sumLeds[_LEDS_SPLIT:]
+
+		for _, v := range led1 {
+			if v == 0 {
+				tmp.WriteString(" ")
+			} else {
+				tmp.WriteString("*")
+			}
+		}
+		tmp.WriteString("[            ]")
+		for _, v := range led2 {
 			if v == 0 {
 				tmp.WriteString(" ")
 			} else {
@@ -44,8 +56,9 @@ func DisplayDriver(display chan ([]byte)) {
 // TODO: real hardware implementation
 // ****
 func SensorDriver(sensorReader chan int, sensors []Sensor) {
-	sensorReader <- 3
 	sensorReader <- 0
+	time.Sleep(1 * time.Second)
+	sensorReader <- 3
 	time.Sleep(9 * time.Second)
 	sensorReader <- 1
 	time.Sleep(7 * time.Second)
