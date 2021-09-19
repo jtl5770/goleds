7d98860f4ee096ad13bfff3d9870dd4934037c7f
Jens Lautenbacher <jtl@gmx.com>
2021-09-19 13:11:43 +0200

    change Led from byte to a full RGB struct.
    Added .IsEmpty() and .Max(Led) helper methods

--- controller/ledproducer.go
+++ controller/ledproducer.go
@@ -6,6 +6,29 @@ type LedProducer interface {
 	Fire()
 }
 
+type Led struct {
+	Red   byte
+	Green byte
+	Blue  byte
+}
+
+func (s Led) IsEmpty() bool {
+	return s.Red == 0 && s.Green == 0 && s.Blue == 0
+}
+
+func (s Led) Max(in Led) Led {
+	if s.Red > in.Red {
+		in.Red = s.Red
+	}
+	if s.Green > in.Green {
+		in.Green = s.Green
+	}
+	if s.Blue > in.Blue {
+		in.Blue = s.Blue
+	}
+	return in
+}
+
 // Local Variables:
 // compile-command: "cd .. && go build"
 // End:
