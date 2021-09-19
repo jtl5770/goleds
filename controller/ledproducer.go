3276ecff34bd255325e593cf4b51d31df6a77c14
Jens Lautenbacher <jtl@gmx.com>
2021-09-19 21:01:21 +0200

    add abstract base struct to hanel some common code for all producers

--- controller/ledproducer.go
+++ controller/ledproducer.go
@@ -1,5 +1,13 @@
 package ledcontroller
 
+import (
+	"math"
+	"sync"
+	t "time"
+)
+
+var NULL_LED = Led{0, 0, 0}
+
 type LedProducer interface {
 	GetLeds() []Led
 	GetUID() string
@@ -16,6 +24,10 @@ func (s Led) IsEmpty() bool {
 	return s.Red == 0 && s.Green == 0 && s.Blue == 0
 }
 
+func (s Led) Intensity() byte {
+	return byte(math.Round(float64(s.Red+s.Green+s.Blue) / 3.0))
+}
+
 func (s Led) Max(in Led) Led {
 	if s.Red > in.Red {
 		in.Red = s.Red
@@ -27,6 +39,40 @@ func (s Led) Max(in Led) Led {
 		in.Blue = s.Blue
 	}
 	return in
+}
+
+type AbstractProducer struct {
+	uid       string
+	leds      []Led
+	isRunning bool
+	lastFire  t.Time
+	// Guards getting and setting LED values
+	ledsMutex sync.Mutex
+	// Guards changes to lastFire & isRunning
+	updateMutex sync.Mutex
+	ledsChanged chan (LedProducer)
+}
+
+// Returns a slice with the current values of all the LEDs.
+// Guarded by s.ledsMutex
+func (s *AbstractProducer) GetLeds() []Led {
+	s.ledsMutex.Lock()
+	defer s.ledsMutex.Unlock()
+	ret := make([]Led, len(s.leds))
+	copy(ret, s.leds)
+	return ret
+}
+
+func (s *AbstractProducer) GetUID() string {
+	return s.uid
+}
+
+// Return the s.lastFire value, guarded by s.updateMutex
+func (s *SensorLedProducer) getLastFire() t.Time {
+	s.updateMutex.Lock()
+	defer s.updateMutex.Unlock()
+
+	return s.lastFire
 }
 
 // Local Variables:
