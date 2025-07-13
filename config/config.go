6c9d999fd459b34d1211c1c85048ab7a72362efd
Jens Lautenbacher <jtl@gmx.com>
2025-07-13 13:17:06 +0200

    refactor(config): improve readability of config struct
    Break out anonymous structs into exported types for each producer
    and other sections. This makes the configuration more modular and
    easier to maintain.

--- config/config.go
+++ config/config.go
@@ -12,79 +12,97 @@ const CONFILE = "config.yml"
 
 var CONFIG Config
 
+// SensorLEDConfig defines the configuration for the SensorLED producer.
+type SensorLEDConfig struct {
+	Enabled      bool          `yaml:"Enabled"`
+	RunUpDelay   time.Duration `yaml:"RunUpDelay"`
+	RunDownDelay time.Duration `yaml:"RunDownDelay"`
+	HoldTime     time.Duration `yaml:"HoldTime"`
+	LedRGB       []float64     `yaml:"LedRGB"`
+}
+
+// NightLEDConfig defines the configuration for the NightLED producer.
+type NightLEDConfig struct {
+	Enabled   bool        `yaml:"Enabled"`
+	Latitude  float64     `yaml:"Latitude"`
+	Longitude float64     `yaml:"Longitude"`
+	LedRGB    [][]float64 `yaml:"LedRGB"`
+}
+
+// HoldLEDConfig defines the configuration for the HoldLED producer.
+type HoldLEDConfig struct {
+	Enabled      bool          `yaml:"Enabled"`
+	HoldTime     time.Duration `yaml:"HoldTime"`
+	TriggerDelay time.Duration `yaml:"TriggerDelay"`
+	TriggerValue int           `yaml:"TriggerValue"`
+	LedRGB       []float64     `yaml:"LedRGB"`
+}
+
+// CylonLEDConfig defines the configuration for the CylonLED producer.
+type CylonLEDConfig struct {
+	Enabled  bool          `yaml:"Enabled"`
+	Duration time.Duration `yaml:"Duration"`
+	Delay    time.Duration `yaml:"Delay"`
+	Step     float64       `yaml:"Step"`
+	Width    int           `yaml:"Width"`
+	LedRGB   []float64     `yaml:"LedRGB"`
+}
+
+// MultiBlobLEDConfig defines the configuration for the MultiBlobLED producer.
+type MultiBlobLEDConfig struct {
+	Enabled  bool          `yaml:"Enabled"`
+	Duration time.Duration `yaml:"Duration"`
+	Delay    time.Duration `yaml:"Delay"`
+	BlobCfg  map[string]struct {
+		DeltaX float64   `yaml:"DeltaX"`
+		X      float64   `yaml:"X"`
+		Width  float64   `yaml:"Width"`
+		LedRGB []float64 `yaml:"LedRGB"`
+	} `yaml:"BlobCfg"`
+}
+
+// HardwareConfig defines the hardware configuration.
+type HardwareConfig struct {
+	LEDType      string `yaml:"LEDType"`
+	SPIFrequency int    `yaml:"SPIFrequency"`
+	Display      struct {
+		ForceUpdateDelay  time.Duration `yaml:"ForceUpdateDelay"`
+		LedsTotal         int           `yaml:"LedsTotal"`
+		ColorCorrection   []float64     `yaml:"ColorCorrection"`
+		APA102_Brightness byte          `yaml:"APA102_Brightness"`
+		LedSegments       map[string][]struct {
+			FirstLed     int    `yaml:"FirstLed"`
+			LastLed      int    `yaml:"LastLed"`
+			SpiMultiplex string `yaml:"SpiMultiplex"`
+			Reverse      bool   `yaml:"Reverse"`
+		} `yaml:"LedSegments"`
+	} `yaml:"Display"`
+	Sensors struct {
+		SmoothingSize int           `yaml:"SmoothingSize"`
+		LoopDelay     time.Duration `yaml:"LoopDelay"`
+		SensorCfg     map[string]struct {
+			LedIndex     int    `yaml:"LedIndex"`
+			SpiMultiplex string `yaml:"SpiMultiplex"`
+			AdcChannel   byte   `yaml:"AdcChannel"`
+			TriggerValue int    `yaml:"TriggerValue"`
+		} `yaml:"SensorCfg"`
+	} `yaml:"Sensors"`
+	SpiMultiplexGPIO map[string]struct {
+		Low  []int `yaml:"Low"`
+		High []int `yaml:"High"`
+	} `yaml:"SpiMultiplexGPIO"`
+}
+
 type Config struct {
-	RealHW     bool
-	SensorShow bool
-	Configfile string
-	SensorLED  struct {
-		Enabled      bool          `yaml:"Enabled"`
-		RunUpDelay   time.Duration `yaml:"RunUpDelay"`
-		RunDownDelay time.Duration `yaml:"RunDownDelay"`
-		HoldTime     time.Duration `yaml:"HoldTime"`
-		LedRGB       []float64     `yaml:"LedRGB"`
-	} `yaml:"SensorLED"`
-	NightLED struct {
-		Enabled   bool        `yaml:"Enabled"`
-		Latitude  float64     `yaml:"Latitude"`
-		Longitude float64     `yaml:"Longitude"`
-		LedRGB    [][]float64 `yaml:"LedRGB"`
-	} `yaml:"NightLED"`
-	HoldLED struct {
-		Enabled      bool          `yaml:"Enabled"`
-		HoldTime     time.Duration `yaml:"HoldTime"`
-		TriggerDelay time.Duration `yaml:"TriggerDelay"`
-		TriggerValue int           `yaml:"TriggerValue"`
-		LedRGB       []float64     `yaml:"LedRGB"`
-	} `yaml:"HoldLED"`
-	CylonLED struct {
-		Enabled  bool          `yaml:"Enabled"`
-		Duration time.Duration `yaml:"Duration"`
-		Delay    time.Duration `yaml:"Delay"`
-		Step     float64       `yaml:"Step"`
-		Width    int           `yaml:"Width"`
-		LedRGB   []float64     `yaml:"LedRGB"`
-	} `yaml:"CylonLED"`
-	MultiBlobLED struct {
-		Enabled  bool          `yaml:"Enabled"`
-		Duration time.Duration `yaml:"Duration"`
-		Delay    time.Duration `yaml:"Delay"`
-		BlobCfg  map[string]struct {
-			DeltaX float64   `yaml:"DeltaX"`
-			X      float64   `yaml:"X"`
-			Width  float64   `yaml:"Width"`
-			LedRGB []float64 `yaml:"LedRGB"`
-		} `yaml:"BlobCfg"`
-	} `yaml:"MultiBlobLED"`
-	Hardware struct {
-		LEDType      string `yaml:"LEDType"`
-		SPIFrequency int    `yaml:"SPIFrequency"`
-		Display      struct {
-			ForceUpdateDelay  time.Duration `yaml:"ForceUpdateDelay"`
-			LedsTotal         int           `yaml:"LedsTotal"`
-			ColorCorrection   []float64     `yaml:"ColorCorrection"`
-			APA102_Brightness byte          `yaml:"APA102_Brightness"`
-			LedSegments       map[string][]struct {
-				FirstLed     int    `yaml:"FirstLed"`
-				LastLed      int    `yaml:"LastLed"`
-				SpiMultiplex string `yaml:"SpiMultiplex"`
-				Reverse      bool   `yaml:"Reverse"`
-			} `yaml:"LedSegments"`
-		} `yaml:"Display"`
-		Sensors struct {
-			SmoothingSize int           `yaml:"SmoothingSize"`
-			LoopDelay     time.Duration `yaml:"LoopDelay"`
-			SensorCfg     map[string]struct {
-				LedIndex     int    `yaml:"LedIndex"`
-				SpiMultiplex string `yaml:"SpiMultiplex"`
-				AdcChannel   byte   `yaml:"AdcChannel"`
-				TriggerValue int    `yaml:"TriggerValue"`
-			} `yaml:"SensorCfg"`
-		} `yaml:"Sensors"`
-		SpiMultiplexGPIO map[string]struct {
-			Low  []int `yaml:"Low"`
-			High []int `yaml:"High"`
-		} `yaml:"SpiMultiplexGPIO"`
-	} `yaml:"Hardware"`
+	RealHW       bool
+	SensorShow   bool
+	Configfile   string
+	SensorLED    SensorLEDConfig    `yaml:"SensorLED"`
+	NightLED     NightLEDConfig     `yaml:"NightLED"`
+	HoldLED      HoldLEDConfig      `yaml:"HoldLED"`
+	CylonLED     CylonLEDConfig     `yaml:"CylonLED"`
+	MultiBlobLED MultiBlobLEDConfig `yaml:"MultiBlobLED"`
+	Hardware     HardwareConfig     `yaml:"Hardware"`
 }
 
 func ReadConfig(cfile string, realhw bool, sensorshow bool) {
